bd7c70424d10c0a7c4d07df777a1b4479cb0053e
shubhamdasgupta725@gamil.com <shubhamdasgupta725@gmail.com>
2025-01-02 21:10:17 +0530

    added sum of two numbers

---
+++ project.php
@@ -0,0 +1,20 @@
+<?php
+function sumOfTwoNumbers(a, b) {
+    return a + b;
+}
+
+
+
+
+const num1 = 5;
+const num2 = 10;
+
+const sum = sumOfTwoNumbers(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
+
+
+
+
+The sum of 5 and 10 is:15
+
+?>
